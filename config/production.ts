f81b9cffa6c0f717f74735b6abf8c74fe364ad4c
div0021 <div2wk@gmail.com>
2024-04-10 14:41:07 +0530

    add scripts for production

---
+++ config/production.ts
@@ -0,0 +1,12 @@
+export default {
+    port:3000,
+    host:"localhost",
+    dbUrl:"",
+    saltWorkFactor:10,
+    accessTokenTtl:"15m",
+    refreshTokenTtl:"23h",
+    publicKey:"",
+    privateKey:"",
+    origin:'http://localhost:5173',
+    googleOAuthRedirectURL:'http://localhost:1337/api/sessions/oauth/google'
+}
